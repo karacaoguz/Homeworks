e32f2dfe63835cae0b5df79cab7cef9ef155b22e
karacaoguz <oguz_krc@hotmail.com>
2023-07-25 14:38:55 +0300

    Fibonacci tamamlandı

--- src/hw11/FibonacciBigInteger.java
+++ src/hw11/FibonacciBigInteger.java
@@ -8,11 +8,13 @@ public class FibonacciBigInteger {
         Scanner scanner = new Scanner(System.in);
         System.out.print("İlk kaç sayıyı görmek istiyorsunuz: ");
         int limit = scanner.nextInt();
+
         BigInteger fibonacci1 = BigInteger.ZERO;
         BigInteger fibonacci2 = BigInteger.ONE;
         BigInteger fibonacci3;
         BigInteger sum;
         System.out.print(fibonacci1.intValue() + " " + fibonacci2.intValue());
+
         for (int i = 2; i < limit; i++) {
             sum = fibonacci2.add(fibonacci1);
             fibonacci3 = sum;
